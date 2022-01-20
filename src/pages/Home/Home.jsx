dda49479b16f1a1f37e03ca813eec68f7a2d9233
Jesus Robles <roblesvargas97@gmail.com>
2022-01-19 20:30:57 -0600

    :construction: work
    work on home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import '../../sassStyles/__tipography.scss'
+import './Home.scss'
+
 const Home = () => {
     return (
-        <section>
+        <section className='Home'>
             <h1>!Realiza tu test del Reglamento de Tránsito¡</h1>
+            <h2>Ingresa con tu nombre</h2>
+            <form className='Home__form'>
+                <input className='form__input' type="text" placeholder='Nombre'/>
+                <input className='form__submit' type="submit" value=''/>
+            </form>
         </section>
     );
 }
