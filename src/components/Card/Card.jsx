9cd5e180c11e4205b2973afd3b7b725377a14347
Jesus Robles <roblesvargas97@gmail.com>
2022-01-23 19:28:37 -0600

    :adhesive_bandage: Image Ornament
    Image Ornament

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -5,8 +5,9 @@ import {FaSignInAlt} from 'react-icons/fa'
 const Card = (props) => {
     return (
         <div className='Card' >
-            <img src={props.image} alt="imagen de referencia" />
+            <img className='Card__img' src={props.image} alt="imagen de referencia" />
             <p>{props.text}</p>
+            <img className='Card__ornament' src="https://i.imgur.com/m69ARJX.png" alt="adorno" />
             {props.button && <button type='button'>Ir ahora <FaSignInAlt/></button>}
         </div>
     );
