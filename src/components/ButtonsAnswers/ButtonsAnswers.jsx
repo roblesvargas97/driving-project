8080e27745b757c0c3fbad240bd07531af8cad4e
Jesus Robles <roblesvargas97@gmail.com>
2022-01-30 20:00:50 -0600

    :lipstick: Add stylet componentTest
    Add styles componentTest

--- src/components/ButtonsAnswers/ButtonsAnswers.jsx
+++ src/components/ButtonsAnswers/ButtonsAnswers.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ButtonsAnswersLogic from './ButtonsAnswersLogic';
-import {BsCircle , BsCheckCircle} from 'react-icons/bs';
+import {BsFillCheckCircleFill} from 'react-icons/bs';
 import './ButtonsAnswers.scss';
 const ButtonsAnswers = ({currentQuestion}) => {
 
@@ -20,7 +20,7 @@ const ButtonsAnswers = ({currentQuestion}) => {
                         <p>{answers[0].answer}</p>
                 </div>
                 <div className='ButtonAnswer__icon'>
-                    <BsCheckCircle color='#fff' size='30px'/>
+                    <BsFillCheckCircleFill color='#fff' size='30px'/>
                 </div>
             </button>
             <button 
@@ -32,7 +32,7 @@ const ButtonsAnswers = ({currentQuestion}) => {
                         <p>{answers[1].answer}</p>
                 </div>
                 <div className='ButtonAnswer__icon'>
-                    <BsCheckCircle color='#fff' size='30px'/>
+                    <BsFillCheckCircleFill color='#fff' size='30px'/>
                 </div>
             </button>
             <button 
@@ -44,7 +44,7 @@ const ButtonsAnswers = ({currentQuestion}) => {
                         <p>{answers[2].answer}</p>
                 </div>
                 <div className='ButtonAnswer__icon'>
-                    <BsCheckCircle color='#fff' size='30px'/>
+                    <BsFillCheckCircleFill color='#fff' size='30px'/>
                 </div>
             </button>
         </React.Fragment>
--- src/pages/ComponentTest/ComponentTest.jsx
+++ src/pages/ComponentTest/ComponentTest.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ComponentTestLogic from './ComponentTestLogic';
 import ButtonsAnswers from '../../components/ButtonsAnswers/ButtonsAnswers';
+import {BsFillCaretRightFill , BsFillCaretLeftFill , BsCheckCircleFill} from 'react-icons/bs';
 import './ComponentTest.scss'
 
 const ComponentTest = () => {
@@ -29,25 +30,31 @@ const ComponentTest = () => {
             <div className='Test__buttons'>
                 <button 
                     type='button' 
+                    className='Test__buttons__prev'
                     onClick={onClickPreviousQuestion} 
                     disabled={state.currentQuestion === 0}>
-                    Anterior
+                        <BsFillCaretLeftFill color='#fff' size='30px'/>
+                        Anterior
                 </button>
                 {
                     state.currentQuestion !== 19 &&
                     <button 
                         type='button' 
+                        className='Test__buttons__next'
                         onClick={onClickNextQuestion} 
                         disabled={state.currentQuestion === 19 || state.stateClick === false }>
-                    siguiente
+                        Siguiente
+                        <BsFillCaretRightFill color='#fff' size='30px'/>
                     </button>
                 }
                 {
                     state.currentQuestion === 19 &&
                     <button 
                         type='button' 
+                        className='Test__buttons__validate'
                         onClick={() => onValidate(userAnswers , pickedQuestions)} >
-                    Validar
+                        Validar
+                        <BsCheckCircleFill color='#fff' size='30px'/>
                 </button>
                 }
             </div>
